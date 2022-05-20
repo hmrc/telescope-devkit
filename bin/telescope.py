5d16274a0e64a5f211f6665615c76bdf578dd45b
Lee Myring <29373851+thinkstack@users.noreply.github.com>
2022-05-20 14:50:12 +0100

    TEL-2675: fix the boolean comparison in entrypoint
    Co-authored-by: Ali Bahman <1422984+webit4me@users.noreply.github.com>

--- bin/telescope.py
+++ bin/telescope.py
@@ -40,7 +40,7 @@ commands = {
 
 
 def is_running_in_docker() -> bool:
-    docker_mode = os.environ.get("TELESCOPE_DEVKIT_DOCKER_MODE", False)
+    docker_mode = os.getenv("TELESCOPE_DEVKIT_DOCKER_MODE", 'False').lower() in ('true', '1', 't', 'y', 'yes')
     if docker_mode:
         return True
 
